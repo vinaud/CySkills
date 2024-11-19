f6771ef72b6b5cd70a1ce778c91dc367379c3af6
Vinaud <leyenasd@gmail.com>
2024-11-19 17:36:50 -0300

    Feature: Login form test

--- playground/cypress/e2e/login.cy.js
+++ playground/cypress/e2e/login.cy.js
@@ -2,5 +2,9 @@ describe('Login', () => {
   it('Should login with success', () => {
     cy.viewport(1920, 1080)
     cy.visit('https://playground.cyskills.com.br')
+    cy.get('[data-cy="email"]').type('papito@cyskills.com.br')
+    cy.get('[data-cy="password"]').type('showtime')
+    cy.get('[data-cy="login-button"]').click()
+    cy.get('[data-cy="welcome-title"]').should('be.visible').and('have.text', 'Boas vindas ao Cypress Playground')
   })
 })
