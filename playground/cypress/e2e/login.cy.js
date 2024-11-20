a8d2072c183e7ae32b419047688edaabc541f3de
Vinaud <leyenasd@gmail.com>
2024-11-20 11:54:01 -0300

    Refactor: added cy commands on login validation to reuse code

--- playground/cypress/e2e/login.cy.js
+++ playground/cypress/e2e/login.cy.js
@@ -7,30 +7,33 @@ describe('Login', () => {
   });
 
   it('Should login with success', () => {
-    cy.get('[data-cy="email"]').type('papito@cyskills.com.br')
-    cy.get('[data-cy="password"]').type('showtime')
-    cy.get('[data-cy="login-button"]').click()
+    cy.login('papito@cyskills.com.br', 'showtime' )
     cy.get('[data-cy="welcome-title"]').should('be.visible').and('have.text', 'Boas vindas ao Cypress Playground')
   })
 
   it('Should not login with wrong password', () => {
-    cy.get('[data-cy="email"]').type('papito@cyskills.com.br')
-    cy.get('[data-cy="password"]').type('s14242424')
-    cy.get('[data-cy="login-button"]').click()
-    cy.get('.notice p').should('be.visible').and('have.text', 'E-mail ou senha incorretos. Por favor, tente novamente!')
+    cy.login('papito@cyskills.com.br', 's14242424' )
+    cy.noticeHave('E-mail ou senha incorretos. Por favor, tente novamente!')
   })
 
   it('Should not login with not registered email', () => {
-    cy.get('[data-cy="email"]').type('404@cyskills.com.br')
-    cy.get('[data-cy="password"]').type('s14242424')
-    cy.get('[data-cy="login-button"]').click()
-    cy.get('.notice p').should('be.visible').and('have.text', 'E-mail ou senha incorretos. Por favor, tente novamente!')
+    cy.login('404@cyskills.com.br', 'showtime' )
+    cy.noticeHave('E-mail ou senha incorretos. Por favor, tente novamente!')
   })
 
   it('Should not login with incorrect email', () => {
-    cy.get('[data-cy="email"]').type('www.com')
-    cy.get('[data-cy="password"]').type('s14242424')
-    cy.get('[data-cy="login-button"]').click()
-    cy.get('.notice p').should('be.visible').and('have.text', 'O formato do e-mail está incorreto. Por favor, verifique e tente novamente!')
+    cy.login('www.com.br', 'showtime' )
+    cy.noticeHave('O formato do e-mail está incorreto. Por favor, verifique e tente novamente!')
   })
 })
+
+Cypress.Commands.add('login', (email, password) => {
+  cy.get('[data-cy="email"]').type(email)
+  cy.get('[data-cy="password"]').type(password)
+  cy.get('[data-cy="login-button"]').click()
+})
+
+Cypress.Commands.add('noticeHave', (text) => {
+  cy.get('.notice p').should('be.visible').and('have.text', text)
+})
+
