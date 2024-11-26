6724b18dcb126c37a6b55360244e3cbfa8389efc
Vinaud <leyenasd@gmail.com>
2024-11-26 15:16:08 -0300

    Feature: Testing multiple select item

---
+++ playground/cypress/e2e/select.cy.js
@@ -0,0 +1,26 @@
+describe('Select', () => {
+
+    beforeEach(() => {
+       cy.goHome()
+       cy.login('papito@cyskills.com.br', 'showtime' )
+       cy.userLoggedIn()
+       cy.goTo('/select', 'Select')
+    });
+   
+    it('Should select the framework Cypress', () => {
+      cy.contains('label', 'Selecione um Framework de Testes')
+      .parent()
+      .find('select')
+      .select('Cypress')
+    });
+    it('Should select the languages which use Node', () => {
+
+       const langs = ['JavaScript', 'TypeScript']
+
+       cy.get('input[placeholder^="Linguagens de programação"]').click()
+       langs.forEach(lang => {
+        cy.contains('.option-item', lang).click()
+       })
+       
+      });
+   });
