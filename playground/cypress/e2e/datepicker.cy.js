a5337bb22dac98d5c43fa247ceda78119b0257b3
Vinaud <leyenasd@gmail.com>
2024-11-27 13:34:31 -0300

    Feature: Testing Date Picker component

---
+++ playground/cypress/e2e/datepicker.cy.js
@@ -0,0 +1,15 @@
+describe('Date Picker', () => {
+    beforeEach(() => {
+        cy.goHome()
+        cy.login('papito@cyskills.com.br', 'showtime' )
+        cy.userLoggedIn()
+        cy.goTo('/date-picker', 'Date Picker')
+     });
+
+     it('Should select a date to input', () => {
+       cy.get('input[placeholder="Escolha uma data"][readonly]').click()
+       cy.get('select[aria-label="Month"]').select('Dezembro')
+       cy.get('input[aria-label="Year"]').type('2024')
+       cy.get('span[aria-label="Dezembro 27, 2024"]').click()
+    });  
+});
