ada4f881478b78f8872ce01b932e26d6ce254316
Vinaud <leyenasd@gmail.com>
2024-11-27 15:30:17 -0300

    Feature: Testing tables

---
+++ playground/cypress/e2e/tables.cy.js
@@ -0,0 +1,48 @@
+describe('Tables', () => {
+    beforeEach(() => {
+        cy.goHome()
+        cy.login('papito@cyskills.com.br', 'showtime' )
+        cy.userLoggedIn()
+        cy.goTo('/tables', 'Tables')
+     });
+
+     it('Should validate Github row', () => {
+       cy.contains('table tbody tr', '1004')
+       .should('be.visible')
+       .and('contain', 'Github')
+       .and('contain', 'papitodev')
+       .and('contain', 'Ativo')
+    });  
+
+    it('Should remove one network', () => {
+
+        const name = 'facebook'
+
+        cy.contains('table tbody tr', '1002')
+        .find('.remove-item')
+        .click()
+
+        cy.contains('button', 'Excluir')
+        .click()
+
+        cy.get('table tbody')
+        .should('not.contain', name)
+        
+     });  
+
+     it('Should keep an item in table after cancel exclusion', () => {
+
+        const name = 'Youtube'
+
+        cy.contains('table tbody tr', '1005')
+        .find('.remove-item')
+        .click()
+
+        cy.contains('button', 'Cancelar')
+        .click()
+
+        cy.get('table tbody')
+        .should('contain', name)
+        
+     });  
+});
