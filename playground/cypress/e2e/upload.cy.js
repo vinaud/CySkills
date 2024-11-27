95ab9377f832d284e3127f0f5a0ff9cabe3107f0
Vinaud <leyenasd@gmail.com>
2024-11-27 09:34:53 -0300

    Feature: Testing file and image upload

---
+++ playground/cypress/e2e/upload.cy.js
@@ -0,0 +1,24 @@
+describe('Upload', () => {
+    beforeEach(() => {
+        cy.goHome()
+        cy.login('papito@cyskills.com.br', 'showtime' )
+        cy.userLoggedIn()
+        cy.goTo('/upload', 'Upload')
+     });
+
+     it('Should attach a file', () => {
+        cy.get('input[name="doc"]').selectFile('cypress/fixtures/doc.pdf')
+        .then(element => {
+            expect(element[0].files[0].name).to.equal('doc.pdf')                
+        })
+    });
+
+    it('Should attach an image', () => {
+        cy.get('input[name="photo"]').selectFile('cypress/fixtures/liga.jpg')
+        .then(element => {
+            expect(element[0].files[0].name).to.equal('liga.jpg')  
+        })
+
+        cy.get('#image-upload').find('img').should('be.visible').and('have.attr', 'src').and('include', 'blob')
+    });
+});
