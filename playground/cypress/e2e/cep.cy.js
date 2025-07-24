3d7c17f5c07304b5aa9730fb0118e24211ae9fa3
Vinaud <leyenasd@gmail.com>
2025-07-24 13:58:01 -0300

    Feature: Use CY Intercept to test metod realying on API call

--- playground/cypress/e2e/cep.cy.js
+++ playground/cypress/e2e/cep.cy.js
@@ -7,10 +7,24 @@ describe('CEP', () => {
      });
 
      it('Should get the address from the Correios API', () => {
-       cy.get('input[name="cep"]').type('04534011')
+
+      const address = {
+        cep: '04534011',
+        logradouro: 'Rua Joaquim Floriano',
+        localidade: 'São Paulo',
+        uf: 'SP'
+      }
+
+      cy.intercept('GET', `https://viacep.com.br/ws/${address.cep}/json/`, {
+        statusCode: 200,
+        body: address
+      }).as('getCep')
+
+       cy.get('input[name="cep"]').type(address.cep)
        cy.contains('button', 'Cadastrar').click()
-       cy.get('input[name="logradouro"]', {timeout: 7000}).should('have.value', 'Rua Joaquim Floriano')
-       cy.get('input[name="cidade"]', {timeout: 7000}).should('have.value', 'São Paulo')
-       cy.get('input[name="estado"]', {timeout: 7000}).should('have.value', 'SP')
+       cy.wait('@getCep')
+       cy.get('input[name="logradouro"]', {timeout: 9000}).should('have.value', address.logradouro)
+       cy.get('input[name="cidade"]', {timeout: 7000}).should('have.value', address.localidade)
+       cy.get('input[name="estado"]', {timeout: 7000}).should('have.value', address.uf)
     });  
 });
