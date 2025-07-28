cf54f9a8171d5650032e2578efe7dc2b9d294fa2
Vinaud <leyenasd@gmail.com>
2025-07-28 09:38:12 -0300

    Test: Validate scenarios with invalid input

--- rockshaver/web/cypress/e2e/pre-cadastro.cy.js
+++ rockshaver/web/cypress/e2e/pre-cadastro.cy.js
@@ -25,4 +25,60 @@ describe('Pre Register', () => {
       .and('have.text', 'fernando@msn.com')
 
   })
+
+  it('Should show error for invalid email', () => {
+    cy.visit('/')
+
+    cy.get('header nav a[href="pre-cadastro"]').click()
+
+    cy.get('input[placeholder="John Doe"]')
+      .type('Teste Usuario')
+
+    cy.get('input[placeholder="john.doe@rockshaver.com"]')
+      .type('email-invalido')
+
+    cy.contains('button[type=submit]', 'Continuar').click()
+
+    cy.contains('O e-mail inserido é inválido').should('be.visible')
+  })
+
+  it('Should show error when only first name is entered', () => {
+    cy.visit('/')
+
+    cy.get('header nav a[href="pre-cadastro"]').click()
+
+    cy.get('input[placeholder="John Doe"]')
+      .type('Fernando')
+
+    cy.get('input[placeholder="john.doe@rockshaver.com"]')
+      .type('fernando@msn.com')
+
+    cy.contains('button[type=submit]', 'Continuar').click()
+
+    cy.contains('label', 'Nome Completo')
+      .parent()
+      .find('.alert-msg')
+      .should('be.visible')
+      .and('include.text', 'Informe seu nome completo')
+  })
+
+  it('Should show errors when both fields are empty', () => {
+    cy.visit('/')
+
+    cy.get('header nav a[href="pre-cadastro"]').click()
+
+    cy.contains('button[type=submit]', 'Continuar').click()
+
+    cy.contains('label', 'Nome Completo')
+    .parent()
+    .find('.alert-msg')
+    .should('be.visible')
+    .and('include.text', 'O campo nome é obrigatório')
+
+    cy.contains('label', 'E-mail')
+    .parent()
+    .find('.alert-msg')
+    .should('be.visible')
+    .and('include.text', 'O campo e-mail é obrigatório')
+  })
 })
