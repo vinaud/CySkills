dca445a4c3504931bd95d0cf2c23b47420c7d706
Vinaud <leyenasd@gmail.com>
2025-07-29 10:28:43 -0300

    Refactor; Refactor tests to apply Actions Feature Project Pattern

--- rockshaver/web/cypress/e2e/pre-cadastro.cy.js
+++ rockshaver/web/cypress/e2e/pre-cadastro.cy.js
@@ -1,42 +1,36 @@
 import preRegPage from "../support/pages/pre-reg.page"
 import homePage from "../support/pages/home.page"
 
+import prereg from "../support/actions/prereg"
+
 describe('Pre Register', () => {
   it('Should Register with success', () => {
     
-    homePage.go()
-    homePage.header.goToPreReg()
-    preRegPage.fillForm('Fernando Papito', 'papito@gmail.com')
-    preRegPage.submit()
+   // homePage.go()
+   // homePage.header.goToPreReg()
+   // preRegPage.fillForm('Fernando Papito', 'papito@gmail.com')
+   // preRegPage.submit()
+   //
+   // homePage.header.verifyPreReg('Fernando', 'papito@gmail.com')
 
-    homePage.header.verifyPreReg('Fernando', 'papito@gmail.com')
+   prereg.start('Fernando Papito', 'papito@gmail.com')
+   prereg.verify('Fernando', 'papito@gmail.com')
   })
 
   it('Should show error for invalid email', () => {
-    homePage.go()
-    homePage.header.goToPreReg()
-    preRegPage.fillForm('Fernando Papito', 'invalid mail')
-    preRegPage.submit()
-
-    preRegPage.alertHave('E-mail', 'O e-mail inserido é inválido')
+    prereg.start('Papito Fernando', 'www.com')
+    prereg.alert('E-mail', 'O e-mail inserido é inválido.') 
     
   })
 
   it('Should show error when only first name is entered', () => {
-    homePage.go()
-    homePage.header.goToPreReg()
-    preRegPage.fillForm('Papito', 'papito@gmail.com')
-    preRegPage.submit()
-
-    preRegPage.alertHave('Nome Completo', 'Informe seu nome completo.')
+    prereg.start('Papito', 'papito#gmail.com')
+    prereg.alert('Nome Completo', 'Informe seu nome completo.') 
   })
 
   it('Should show errors when both fields are empty', () => {
-    homePage.go()
-    homePage.header.goToPreReg()
-    preRegPage.submit()
-
-   preRegPage.alertHave('Nome Completo', 'O campo nome é obrigatório')
-   preRegPage.alertHave('E-mail', 'O campo e-mail é obrigatório')
+   prereg.start()
+   prereg.alert('Nome Completo', 'O campo nome é obrigatório.')
+   prereg.alert('E-mail', 'O campo e-mail é obrigatório.')  
   })
 })
