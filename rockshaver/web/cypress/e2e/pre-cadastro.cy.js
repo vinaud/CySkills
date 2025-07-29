4b00f3e5fe9fe5254015366974d32a8228092b79
Vinaud <leyenasd@gmail.com>
2025-07-29 11:09:00 -0300

    Refactor: update project to use cypress commands as pattern

--- rockshaver/web/cypress/e2e/pre-cadastro.cy.js
+++ rockshaver/web/cypress/e2e/pre-cadastro.cy.js
@@ -1,36 +1,21 @@
-import preRegPage from "../support/pages/pre-reg.page"
-import homePage from "../support/pages/home.page"
-
-import prereg from "../support/actions/prereg"
-
 describe('Pre Register', () => {
   it('Should Register with success', () => {
-    
-   // homePage.go()
-   // homePage.header.goToPreReg()
-   // preRegPage.fillForm('Fernando Papito', 'papito@gmail.com')
-   // preRegPage.submit()
-   //
-   // homePage.header.verifyPreReg('Fernando', 'papito@gmail.com')
-
-   prereg.start('Fernando Papito', 'papito@gmail.com')
-   prereg.verify('Fernando', 'papito@gmail.com')
+   cy.startPreRegistration('Fernando Papito', 'papito@gmail.com')
+   cy.verifyPreRegistered('Fernando', 'papito@gmail.com')
   })
-
   it('Should show error for invalid email', () => {
-    prereg.start('Papito Fernando', 'www.com')
-    prereg.alert('E-mail', 'O e-mail inserido é inválido.') 
-    
+    cy.startPreRegistration('Papito Fernando', 'www.com')
+    cy.alertHave('E-mail', 'O e-mail inserido é inválido.') 
   })
 
   it('Should show error when only first name is entered', () => {
-    prereg.start('Papito', 'papito#gmail.com')
-    prereg.alert('Nome Completo', 'Informe seu nome completo.') 
+    cy.startPreRegistration('Papito', 'papito#gmail.com')
+    cy.alertHave('Nome Completo', 'Informe seu nome completo.') 
   })
 
   it('Should show errors when both fields are empty', () => {
-   prereg.start()
-   prereg.alert('Nome Completo', 'O campo nome é obrigatório.')
-   prereg.alert('E-mail', 'O campo e-mail é obrigatório.')  
+    cy.startPreRegistration()
+    cy.alertHave('Nome Completo', 'O campo nome é obrigatório.')
+    cy.alertHave('E-mail', 'O campo e-mail é obrigatório.')  
   })
 })
--- rockshaver/web/cypress/support/commands.js
+++ rockshaver/web/cypress/support/commands.js
@@ -23,3 +23,44 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('startPreRegistration', (fullname = '', email = '') => {
+    cy.visit('/')
+    cy.get('header nav a[href="pre-cadastro"]').click();
+
+    cy.get('form h2')
+        .should('be.visible')
+        .and('have.text', 'Seus dados');
+
+    cy.get('input[name="fullname"]').as('fullname')
+    cy.get('input[name="email"]').as('email')
+
+    if (fullname) {
+        cy.get('@fullname')
+            .type(fullname);
+    }
+    if (email) {
+        cy.get('@email')
+            .type(email);
+    }
+
+    cy.contains('button[type=submit]', 'Continuar').click();
+})
+
+Cypress.Commands.add('verifyPreRegistered', (firstName, email) => {
+    cy.get('.user-name')
+        .should('be.visible')
+        .and('have.text', `Olá, ${firstName}`);
+
+    cy.get('.user-email')
+        .should('be.visible')
+        .and('have.text', email);
+})
+
+Cypress.Commands.add('alertHave', (fieldName, text) => {
+    cy.contains('label', fieldName)
+        .parent()
+        .find('.alert-msg')
+        .should('be.visible')
+        .and('have.text', text);
+})
