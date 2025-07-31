5c7d9d7ad593e2e21b89496830865fce98ade5b9
Vinaud <leyenasd@gmail.com>
2025-07-31 09:22:13 -0300

    Test: E2E test of scheduling function

--- rockshaver/web/cypress/e2e/agendamento.cy.js
+++ rockshaver/web/cypress/e2e/agendamento.cy.js
@@ -17,6 +17,19 @@ describe('Agendamento', () => {
 
         cy.contains('span', 'Serviços').should('be.visible')
         
+        cy.contains('div', 'Combo').parent().click()
+
+        cy.contains('span', 'Dias Disponíveis').should('be.visible')
+        cy.contains('span', 'Horários Disponíveis').should('be.visible')
+
+        cy.contains('.dia-semana', '1').parent().click()
+
+        cy.contains('.hora-opcao', '11:00').click()
+
+        cy.contains('button', 'Confirmar e reservar').click()
+
+        cy.get('h3').should('be.visible').and('have.text', 'Tudo certo por aqui! Seu horário está confirmado.')
+        
     })
 
 })
